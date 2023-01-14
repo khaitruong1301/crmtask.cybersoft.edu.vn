a95bf3d11819848aea2b0e5b5b9d8fe8139d0358
hoang <hoang.nhh1991@gmail.com>
2023-01-14 09:30:28 +0700

    update hoangdev

---
+++ src/types/TypeDataRoadMap.tsx
@@ -0,0 +1,9 @@
+export interface RoadMapDetail {
+    id:               number;
+    tenRoadMapDetail: string;
+    biDanh:           string;
+    stt:              number;
+    danhSachSkill:    string[];
+    endRoadMap:       boolean;
+    maRoadMap:        number;
+}
